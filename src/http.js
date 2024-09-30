7ebfa946b5fc0e39693bcd6aa677d778b9fda1f0
ThangMai81 <maithang18122003@gmail.com>
2024-09-30 15:37:32 +0700

    Update from v2 -> v4

--- src/http.js
+++ src/http.js
@@ -8,7 +8,6 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchSearch: `/search/movie?api_key=${API_KEY}&language=en-US`,
 };
 const lengthRequests = Object.values(requests).length;
 console.log("lengthRequests: ", lengthRequests);
